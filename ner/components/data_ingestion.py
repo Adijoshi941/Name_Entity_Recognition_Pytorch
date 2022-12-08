bf088e6e7f7bab216c9fbd4510b7909a6ed8929d
Adijoshi941 <ajoshi941@gmail.com>
2022-12-08 14:29:56 +0530

    Data Ingestion Updated

--- ner/components/data_ingestion.py
+++ ner/components/data_ingestion.py
@@ -1,10 +1,10 @@
-from datasets import load_dataset
+=from datasets import load_dataset
 from ner.exception.exception import CustomException
 from ner.entity.config_entity import DataIngestionConfig
 from ner.config.configurations import Configuration
 import logging
 import sys
-
+import os
 logger = logging.getLogger(__name__)
 
 
@@ -20,25 +20,44 @@ class DataIngestion:
             Cross-lingual Transfer Evaluation of Multilingual Encoders 
             (XTREME) benchmark called WikiANN or PAN-X.
             
+            It first checks if data is available in artifacts data_load folder.
+            If data is not available then it will load from hugging face library else it will load from local path.
             Returns: Dict of train test validation data 
             """
+            if self.check_data():
+                #work in progress
+                return 0
 
-            # Create method to check if data exists in data_store before downloading. 
-            logger.info(f"Loading Data from Hugging face ")
-            pan_en_data = load_dataset(self.data_ingestion_config.dataset_name,
-                                       name=self.data_ingestion_config.subset_name)
-            logger.info(f"Dataset Info : {pan_en_data}")
+            else:
+                logger.info(f"Loading Data from Hugging face ")
+                pan_en_data = load_dataset(self.data_ingestion_config.dataset_name,
+                                        name=self.data_ingestion_config.subset_name)
+                logger.info(f"Dataset Info : {pan_en_data}")
 
-            return pan_en_data
+                return pan_en_data
 
         except Exception as e:
             message = CustomException(e, sys)
             logger.error(message.error_message)
 
+    def check_data(path):
+        try:
+
+            if os.path.exists(path) and not os.path.isfile(path):
+        
+                # Checking if the directory is empty or not
+                if not os.listdir(path):
+                    return 0
+                else:
+                    return 1
+            else:
+                return 0
+
+        except Exception as e:
+            message = CustomException(e, sys)
+            logger.error(message.error_message)
 
 if __name__ == "__main__":
     project_config = Configuration()
     ingestion = DataIngestion(project_config.get_data_ingestion_config())
-    #print(ingestion)
-    #data = ingestion.get_data()
-    #print(data)
+    print(ingestion)
